e17acd6f4350975d14406f952064ee4ea4492975
macnick <nick@macexperts.gr>
2019-12-28 13:16:50 +0200

    Add stock model

---
+++ app/models/stock.rb
@@ -0,0 +1,21 @@
+class Stock < ApplicationRecord
+  has_many :user_stocks
+  has_many :users, through: :user_stocks
+ 
+ 
+  def self.find_by_ticker(ticker_symbol)
+    where(ticker: ticker_symbol).first
+  end
+ 
+ 
+  def self.new_from_lookup(ticker_symbol)
+    begin
+      client = IEX::Api::Client.new(publishable_token: "pk_bb4e31c71dd640bb9e028d8fed14cce7")
+      looked_up_stock = client.quote(ticker_symbol)
+      new(name: looked_up_stock.company_name,
+          ticker: looked_up_stock.symbol, last_price: looked_up_stock.latest_price)
+    rescue Exception => e
+      return nil
+    end
+  end
+end
