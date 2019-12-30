c9b9bd2856d2808ce9c23bb7a2f4b06f70d6beb5
macnick <nick@macexperts.gr>
2019-12-30 14:46:05 +0200

    Fix stock updates and invalid search results

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,6 +1,16 @@
 class StocksController < ApplicationController
   def search
-    @stock = Stock.new_from_lookup(params[:stock])
-    render 'users/my_portfolio'
+    if params[:stock].present?
+      @stock = Stock.new_from_lookup(params[:stock])
+      if @stock
+        render 'users/my_portfolio'
+      else
+        flash[:danger] = "This stock symbol does not exist."
+        redirect_to my_portfolio_path
+      end
+    else
+      flash[:danger] = "You didn't enter any symbol"
+      redirect_to my_portfolio_path
+    end
   end
 end
