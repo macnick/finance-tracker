117934b393511633989f8ec9a6a496648d7af8d6
macnick <nick@macexperts.gr>
2019-12-31 12:23:25 +0200

    Fix error messages and refactor StockController

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,18 +1,17 @@
 class StocksController < ApplicationController
+  
   def search
-    if params[:stock].present?
-      @stock = Stock.new_from_lookup(params[:stock])
-      if @stock
-        respond_to do |format|
-          format.js { render partial: 'users/result' }
-        end
-      else
-        flash[:danger] = "This stock symbol does not exist."
-        redirect_to my_portfolio_path
-      end
+    if params[:stock].blank?
+      flash.now[:danger] = "You didn't enter any symbol  "
     else
-      flash[:danger] = "You didn't enter any symbol"
-      redirect_to my_portfolio_path
+      @stock = Stock.new_from_lookup(params[:stock])
+      flash.now[:danger] = "This stock symbol does not exist  " unless @stock
     end
+    render partial: 'users/result'
   end
 end
+
+
+# respond_to do |format|
+#   format.js { render partial: 'users/result' }
+# end
