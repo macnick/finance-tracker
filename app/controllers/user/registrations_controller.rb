3c415abd7db33382e39f6dcba207c5852fece31d
macnick <nick@macexperts.gr>
2020-01-02 20:23:04 +0200

    Add UI views for users, fix the navigation to a partial. Add styling

--- app/controllers/user/registrations_controller.rb
+++ app/controllers/user/registrations_controller.rb
@@ -4,7 +4,7 @@ class User::RegistrationsController < Devise::RegistrationsController
   private
   # protected ? 
   def configure_permitted_parameters
-    devise_parameter_sanitize.permit(:sign_up, keys: [:first_name, :last_name])
-    devise_parameter_sanitize.permit(:account_update, keys: [:first_name, :last_name   ])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name   ])
   end
 end
