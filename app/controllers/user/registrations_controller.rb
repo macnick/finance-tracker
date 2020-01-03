1b4499874fdf1ec14f1b10665a13badd77d9081c
macnick <nick@macexperts.gr>
2020-01-03 12:02:39 +0200

    Add font-awesome-rails gem

--- app/controllers/user/registrations_controller.rb
+++ app/controllers/user/registrations_controller.rb
@@ -5,6 +5,6 @@ class User::RegistrationsController < Devise::RegistrationsController
   # protected ? 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name   ])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name])
   end
 end
