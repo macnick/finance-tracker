f0a3ebe7b1929189425e38e0a5148247be466099
macnick <nick@macexperts.gr>
2020-01-03 13:08:29 +0200

    Add friendship views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,6 @@ class UsersController < ApplicationController
   end
 
   def my_friends
-
+    @friendships = current_user.friends
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
   get 'search_stocks', to: 'stocks#search'
   get 'my_friends', to: 'users#my_friends'
   resources :user_stocks, only: [:create, :destroy]
+  resources :user, only: [:show]
+  resources :friendship
 end
