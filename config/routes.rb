4fc4326861dc6f1d5866ae169db0e1e301a492f6
macnick <nick@macexperts.gr>
2020-01-04 19:58:54 +0200

    Search and display users

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,10 @@ Rails.application.routes.draw do
   root 'welcome#index'
   get 'my_portfolio', to: 'users#my_portfolio'
   get 'search_stocks', to: 'stocks#search'
-  get 'my_friends', to: 'users#my_friends'
   resources :user_stocks, only: [:create, :destroy]
   resources :user, only: [:show]
   resources :friendships
+  get 'my_friends', to: 'users#my_friends'
   get 'search_friends', to: 'users#search'
+  post 'add_friend', to: 'user#add_friend'
 end
