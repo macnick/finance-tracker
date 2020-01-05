c75c783179ea72204b58b66fb061360d98d727e7
Nick Haras <nick@macexperts.gr>
2020-01-05 19:53:03 +0200

    Fix add user bug

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,5 @@ Rails.application.routes.draw do
   resources :friendships
   get 'my_friends', to: 'users#my_friends'
   get 'search_friends', to: 'users#search'
-  post 'add_friend', to: 'user#add_friend'
+  post 'add_friend', to: 'users#add_friend'
 end
